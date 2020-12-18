fd7e06704b2e9d3bdce338264926d94b5ed0f0ce
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-18 23:53:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function createEmployeeRecord (employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
